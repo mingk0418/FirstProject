27266fec09502fcccefbe5cf56c5dc7bda0119ed
SSang <SSang@sist58>
2018-08-28 15:31:26 +0900

    first commit

--- 20180828_GitHubProject/src/com/sist/git/MainClass.java
+++ 20180828_GitHubProject/src/com/sist/git/MainClass.java
@@ -4,7 +4,11 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1");
+		System.out.println("Hello GitHub!!!");
+		System.out.println("Hello GitHub!!!");
+		System.out.println("Hello GitHub!!!");
+		System.out.println("Hello GitHub!!!");
+		System.out.println("Hello GitHub!!!");
 	}
 
 }
